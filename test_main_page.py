9120308bd36a0dad8e94c9b08466f86dd7b963d8
Artur Valter <valterartour@ukr.net>
2020-03-16 16:52:04 +0200

    Locators added, class for testing login page added

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
 from .page.main_page import MainPage
+from .page.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()    
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
